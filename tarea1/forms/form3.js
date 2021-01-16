eaa38cdd7e0a1bdd4d6e8a726adf9c25576ff4ec
SamSerra7 <samsserranoguerra7@gmail.com>
2021-01-16 17:56:15 -0600

    Third and Fourth Form, to know learning type and gender

--- tarea1/forms/form3.js
+++ tarea1/forms/form3.js
@@ -15,7 +15,142 @@ function Form3(){
 
 
 function createForm3(root){
-    let questions = document.createElement("h1");
-    questions.innerText="Formulario 3";
-    root.appendChild(questions);
+    //Page title
+    let title = document.createElement("h1");
+    title.innerText="Formulario 3";
+    title.classList.add("title");
+    
+    //Subtitle
+    let subtitle = document.createElement("h2");
+    subtitle.innerText = "De qué género eres...";
+    subtitle.classList.add("subtitle");
+
+    // div container
+    let container = document.createElement("div");
+    container.classList.add("opts-container");
+
+    //option learning type
+    let opt = document.createElement("div");
+    let lbl = document.createElement("h5");
+    lbl.innerText = "Tipo de aprendizaje";
+    //options
+    let opt_sel = document.createElement("select");
+    opt_sel.id = "learningType";
+    let opt_1 = document.createElement("option");
+    opt_1.innerText = "divergente";
+    opt_1.value = "divergente"; 
+    let opt_2 = document.createElement("option");
+    opt_2.innerText = "convergente";
+    opt_2.value = "convergente";
+    let opt_3 = document.createElement("option");
+    opt_3.innerText = "asimilador";
+    opt_3.value = "asimilador";
+    let opt_4 = document.createElement("option");
+    opt_4.innerText = "acomodador";
+    opt_4.value = "acomodador";
+
+    opt_sel.appendChild(opt_1);
+    opt_sel.appendChild(opt_2);
+    opt_sel.appendChild(opt_3);
+    opt_sel.appendChild(opt_4);
+
+    opt.appendChild(lbl);
+    opt.appendChild(opt_sel);
+    
+
+    //select for the average
+    let average = document.createElement("div");
+    let lbl_average = document.createElement("h5");
+    lbl_average.innerText = "Promedio ";
+    let input = document.createElement("input");
+    input.id = "average";
+    input.type = "number";
+    average.appendChild(lbl_average);
+    average.appendChild(input);
+
+
+    //option branch
+    let opt_branch = document.createElement("div");
+    let lbl_branch = document.createElement("h5");
+    lbl_branch.innerText = "Recinto";
+    //options
+    let opt_sel_branch = document.createElement("select");
+    opt_sel_branch.id = "branch";
+    let opt_1_branch = document.createElement("option");
+    opt_1_branch.innerText = "Paraíso";
+    opt_1_branch.value = "Paraiso"; 
+    let opt_2_branch = document.createElement("option");
+    opt_2_branch.innerText = "Turrialba";
+    opt_2_branch.value = "Turrialba";
+
+    opt_sel_branch.appendChild(opt_1_branch);
+    opt_sel_branch.appendChild(opt_2_branch);
+
+    opt_branch.appendChild(lbl_branch);
+    opt_branch.appendChild(opt_sel_branch);
+    
+
+
+    //add rows to container
+    container.appendChild(opt);
+    container.appendChild(document.createElement("hr"))//space between
+    container.appendChild(average);
+    container.appendChild(document.createElement("hr"))//space between
+    container.appendChild(opt_branch);
+    
+    
+    //button to trigger the calculation
+    let button = document.createElement("button");
+    button.type= "submit";
+    button.innerText = "Calcular"
+    button.addEventListener("click",() => calculate(root));
+    button.classList.add("btn-calculate");
+    
+
+    //adds childs to the root
+    root.appendChild(title); //add title to the root
+    root.appendChild(subtitle); //add subtitle to the root
+    root.appendChild(container); //add options to the root
+    root.appendChild(button); //add button to the root
+}
+
+
+
+
+
+//make the euclidean calculation
+function calculate(root){
+
+    let currentGender = "";
+    let currentDistance;
+
+    let learningType = document.getElementById("learningType").value;
+    let average = document.getElementById("average").value;
+    let branch = document.getElementById("branch").value;
+
+    
+    //evaluate all variables against the JSON
+    getJSON()["EstiloSexoPromedioRecinto"].map(element => {
+        let euclidean = Math.sqrt( 
+                                    ( ( learningType === element["Estilo"].toLowerCase()? 0.001 : 1 ) ^ 2 ) + 
+                                    ( ( average - parseFloat(element["Promedio"]) ) ^ 2 ) + 
+                                    ( ( branch === element["Recinto"] ? 0.001 : 1 ) ^ 2 ) 
+                                );
+    
+        if(currentDistance === undefined || euclidean < currentDistance){
+            currentDistance = euclidean;
+            currentGender = element["Sexo"];
+        }                   
+    });
+
+    let result = document.getElementById("result")
+    while(result.firstChild){
+        result.removeChild(result.firstChild);
+    }
+    let gender = currentGender.toLowerCase() === "m" ? "Masculino": "Femenino";
+    let finalStr = document.createElement("h1");
+    finalStr.innerText = "Tu género es, probablemente: "+ gender;
+    finalStr.style= "margin-top: 3%;";
+    result.appendChild(finalStr);
+    window.scrollTo(0,document.body.scrollHeight);//scroll to bottom
 }
--- tarea1/forms/form4.js
+++ tarea1/forms/form4.js
@@ -14,7 +14,134 @@ function Form4(){
 
 
 function createForm4(root){
-    let questions = document.createElement("h1");
-    questions.innerText="Formulario 4";
-    root.appendChild(questions);
+    //Page title
+    let title = document.createElement("h1");
+    title.innerText="Formulario 4";
+    title.classList.add("title");
+    
+    //Subtitle
+    let subtitle = document.createElement("h2");
+    subtitle.innerText = "Yo aprendo...";
+    subtitle.classList.add("subtitle");
+
+    // div container
+    let container = document.createElement("div");
+    container.classList.add("opts-container");
+
+   
+    //option branch
+    let opt_branch = document.createElement("div");
+    let lbl_branch = document.createElement("h5");
+    lbl_branch.innerText = "Recinto";
+    //options
+    let opt_sel_branch = document.createElement("select");
+    opt_sel_branch.id = "branch";
+    let opt_1_branch = document.createElement("option");
+    opt_1_branch.innerText = "Paraíso";
+    opt_1_branch.value = "Paraiso"; 
+    let opt_2_branch = document.createElement("option");
+    opt_2_branch.innerText = "Turrialba";
+    opt_2_branch.value = "Turrialba";
+
+    opt_sel_branch.appendChild(opt_1_branch);
+    opt_sel_branch.appendChild(opt_2_branch);
+
+    opt_branch.appendChild(lbl_branch);
+    opt_branch.appendChild(opt_sel_branch);
+    
+
+    //select for the average
+    let average = document.createElement("div");
+    let lbl_average = document.createElement("h5");
+    lbl_average.innerText = "Promedio ";
+    let input = document.createElement("input");
+    input.id = "average";
+    input.type = "number";
+    average.appendChild(lbl_average);
+    average.appendChild(input);
+
+
+    //option gender
+    let opt_gender = document.createElement("div");
+    let lbl_gender = document.createElement("h5");
+    lbl_gender.innerText = "Sexo";
+    //options
+    let opt_sel_gender = document.createElement("select");
+    opt_sel_gender.id = "gender";
+    let opt_1_gender = document.createElement("option");
+    opt_1_gender.innerText = "F";
+    opt_1_gender.value = "F"; 
+    let opt_2_gender = document.createElement("option");
+    opt_2_gender.innerText = "M";
+    opt_2_gender.value = "M";
+
+    opt_sel_gender.appendChild(opt_1_gender);
+    opt_sel_gender.appendChild(opt_2_gender);
+
+    opt_gender.appendChild(lbl_gender);
+    opt_gender.appendChild(opt_sel_gender);
+    
+
+
+    //add rows to container
+    container.appendChild(opt_branch);
+    container.appendChild(document.createElement("hr"))//space between
+    container.appendChild(average);
+    container.appendChild(document.createElement("hr"))//space between
+    container.appendChild(opt_gender);
+    
+    
+    //button to trigger the calculation
+    let button = document.createElement("button");
+    button.type= "submit";
+    button.innerText = "Calcular"
+    button.addEventListener("click",() => calculate(root));
+    button.classList.add("btn-calculate");
+    
+
+    //adds childs to the root
+    root.appendChild(title); //add title to the root
+    root.appendChild(subtitle); //add subtitle to the root
+    root.appendChild(container); //add options to the root
+    root.appendChild(button); //add button to the root
+}
+
+
+
+
+
+//make the euclidean calculation
+function calculate(root){
+
+   let currentLearningType = "";
+   let currentDistance;
+
+   let branch = document.getElementById("branch").value;
+   let average = document.getElementById("average").value;
+   let gender = document.getElementById("gender").value;
+
+   
+   //evaluate all variables against the JSON
+   getJSON()["EstiloSexoPromedioRecinto"].map(element => {
+       let euclidean = Math.sqrt( 
+                                   ( ( branch === element["Recinto"].toLowerCase()? 0.001 : 1 ) ^ 2 ) + 
+                                   ( ( average - parseFloat(element["Promedio"]) ) ^ 2 ) + 
+                                   ( ( gender === element["Sexo"] ? 0.001 : 1 ) ^ 2 ) 
+                               );
+   
+       if(currentDistance === undefined || euclidean < currentDistance){
+           currentDistance = euclidean;
+           currentLearningType = element["Estilo"];
+       }                   
+   });
+
+   let result = document.getElementById("result")
+   while(result.firstChild){
+       result.removeChild(result.firstChild);
+   }
+   let finalStr = document.createElement("h1");
+   finalStr.innerText = "Tu estilo de aprendizaje es, probablemente: "+currentLearningType;
+   finalStr.style= "margin-top: 3%;";
+   result.appendChild(finalStr);
+   window.scrollTo(0,document.body.scrollHeight);//scroll to bottom
 }
